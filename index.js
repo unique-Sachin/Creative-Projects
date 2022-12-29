7e5fa64cdcb834c533bec3f66f7d89a18041209d
Sachin Mishra <msachin786.pm@gmail.com>
2022-12-30 01:39:57 +0530

    updated index
    new changes did
    
    Co-Authored-By: Hanumat Sharan <112655049+HANUMAT-SHARAN@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-let name = "sachin"
-let age = 55
-let school = "joseph"
+let name = "sachin";
+let age = 55;
+let school = "joseph";
+
+console.log("hello");
+
+function myfun(int) {
+  console.log(int);
+}
